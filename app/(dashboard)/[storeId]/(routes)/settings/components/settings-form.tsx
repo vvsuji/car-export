5df0a4a07cdfed581d3f62ce37a75163a9f5d78c
vvsuji <suji.falcon@gmail.com>
2023-11-23 14:31:31 -0500

    settings started, styling added

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { Heading } from '@/components/ui/heading';
+import { Store } from '@prisma/client';
+
+interface SettingsFormProps {
+	initialData: Store;
+}
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+	return (
+		<div className='flex items-center justify-between'>
+			<Heading title='Settings' description='Manage store preferences' />
+		</div>
+	);
+};
