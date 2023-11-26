7e0751c2776f94d4e41bad2266ebf5bafbd8ea14
vvsuji <suji.falcon@gmail.com>
2023-11-25 19:17:39 -0500

    hmmmmmmx2

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -28,23 +28,23 @@ const buttonVariants = cva(
 		},
 		defaultVariants: {
 			variant: 'default',
-			make: 'default',
+			size: 'default',
 		},
 	},
 );
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+		VariantProps<typeof buttonVariants> {
+	asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-	({ className, variant, make, asChild = false, ...props }, ref) => {
+	({ className, variant, size, asChild = false, ...props }, ref) => {
 		const Comp = asChild ? Slot : 'button';
 		return (
 			<Comp
-				className={cn(buttonVariants({ variant, make, className }))}
+				className={cn(buttonVariants({ variant, size, className }))}
 				ref={ref}
 				{...props}
 			/>
