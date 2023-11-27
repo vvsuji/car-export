116749f37b343f1d64546df604c33bb3031f2515
vvsuji <suji.falcon@gmail.com>
2023-11-27 14:06:41 -0500

    fuel type and drive type added

---
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -0,0 +1,117 @@
+import prismadb from '@/lib/prismadb';
+
+import { ProductForm } from './components/product-form';
+
+const ProductPage = async ({
+	params,
+}: {
+	params: { productId: string; storeId: string };
+}) => {
+	const product = await prismadb.product.findUnique({
+		where: {
+			id: params.productId,
+		},
+		include: {
+			images: true,
+		},
+	});
+
+	const categories = await prismadb.category.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+
+	const makes = await prismadb.make.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+
+	const colors = await prismadb.color.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+	const conditions = await prismadb.condition.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+	const driveTypes = await prismadb.driveType.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+	const engineVolumes = await prismadb.engineVolume.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+	const fuelTypes = await prismadb.fuelType.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+	const locations = await prismadb.location.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+	const models = await prismadb.model.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+	const options = await prismadb.option.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+	const passengers = await prismadb.passenger.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+	const steerings = await prismadb.steering.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+	const transmissions = await prismadb.transmission.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+	const years = await prismadb.year.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+
+	return (
+		<div className='flex-col'>
+			<div className='flex-1 space-y-4 p-8 pt-6'>
+				<ProductForm
+					categories={categories}
+					colors={colors}
+					makes={makes}
+					conditions={conditions}
+					driveTypes={driveTypes}
+					engineVolumes={engineVolumes}
+					fuelTypes={fuelTypes}
+					locations={locations}
+					models={models}
+					options={options}
+					passengers={passengers}
+					steerings={steerings}
+					transmissions={transmissions}
+					years={years}
+					initialData={product}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default ProductPage;
