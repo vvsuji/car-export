4226e353ccd3aadc661863eac7e7d1e3c7bdba43
vvsuji <suji.falcon@gmail.com>
2023-11-27 21:20:47 -0500

    ok

--- app/(dashboard)/[storeId]/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/products/[productId]/components/product-form.tsx
@@ -1,394 +1,249 @@
-'use client';
+import { NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs';
 
-import * as z from 'zod';
-import axios from 'axios';
-import { useState } from 'react';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { toast } from 'react-hot-toast';
-import { Trash } from 'lucide-react';
-import { Category, Color, Image, Product, Make } from '@prisma/client';
-import { useParams, useRouter } from 'next/navigation';
+import prismadb from '@/lib/prismadb';
 
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from '@/components/ui/form';
-import { Separator } from '@/components/ui/separator';
-import { Heading } from '@/components/ui/heading';
-import { AlertModal } from '@/components/modals/alert-modal';
-import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from '@/components/ui/select';
-import ImageUpload from '@/components/ui/image-upload';
-import { Checkbox } from '@/components/ui/checkbox';
+export async function GET(
+	req: Request,
+	{ params }: { params: { productId: string } },
+) {
+	try {
+		if (!params.productId) {
+			return new NextResponse('Product id is required', { status: 400 });
+		}
 
-const formSchema = z.object({
-	name: z.string().min(1),
-	images: z.object({ url: z.string() }).array(),
-	price: z.coerce.number().min(1),
-	categoryId: z.string().min(1),
-	colorId: z.string().min(1),
-	makeId: z.string().min(1),
-	isFeatured: z.boolean().default(false).optional(),
-	isArchived: z.boolean().default(false).optional(),
-});
+		const product = await prismadb.product.findUnique({
+			where: {
+				id: params.productId,
+			},
+			include: {
+				images: true,
+				category: true,
+				make: true,
+				color: true,
+				condition: true,
+				driveType: true,
+				engineVolume: true,
+				fuelType: true,
+				location: true,
+				model: true,
+				option: true,
+				passenger: true,
+				steering: true,
+				transmission: true,
+				year: true,
+			},
+		});
 
-type ProductFormValues = z.infer<typeof formSchema>;
-
-interface ProductFormProps {
-	initialData:
-		| (Product & {
-				images: Image[];
-		  })
-		| null;
-	categories: Category[];
-	colors: Color[];
-	makes: Make[];
+		return NextResponse.json(product);
+	} catch (error) {
+		console.log('[PRODUCT_GET]', error);
+		return new NextResponse('Internal error', { status: 500 });
+	}
 }
 
-export const ProductForm: React.FC<ProductFormProps> = ({
-	initialData,
-	categories,
-	makes,
-	colors,
-}) => {
-	const params = useParams();
-	const router = useRouter();
+export async function DELETE(
+	req: Request,
+	{ params }: { params: { productId: string; storeId: string } },
+) {
+	try {
+		const { userId } = auth();
 
-	const [open, setOpen] = useState(false);
-	const [loading, setLoading] = useState(false);
-
-	const title = initialData ? 'Edit product' : 'Create product';
-	const description = initialData ? 'Edit a product.' : 'Add a new product';
-	const toastMessage = initialData ? 'Product updated.' : 'Product created.';
-	const action = initialData ? 'Save changes' : 'Create';
-
-	const defaultValues = initialData
-		? {
-				...initialData,
-				price: parseFloat(String(initialData?.price)),
-		  }
-		: {
-				name: '',
-				images: [],
-				price: 0,
-				categoryId: '',
-				colorId: '',
-				makeId: '',
-				isFeatured: false,
-				isArchived: false,
-		  };
-
-	const form = useForm<ProductFormValues>({
-		resolver: zodResolver(formSchema),
-		defaultValues,
-	});
-
-	const onSubmit = async (data: ProductFormValues) => {
-		try {
-			setLoading(true);
-			if (initialData) {
-				await axios.patch(
-					`/api/${params.storeId}/products/${params.productId}`,
-					data,
-				);
-			} else {
-				await axios.post(`/api/${params.storeId}/products`, data);
-			}
-			router.refresh();
-			router.push(`/${params.storeId}/products`);
-			toast.success(toastMessage);
-		} catch (error: any) {
-			toast.error('Something went wrong.');
-		} finally {
-			setLoading(false);
+		if (!userId) {
+			return new NextResponse('Unauthenticated', { status: 403 });
 		}
-	};
 
-	const onDelete = async () => {
-		try {
-			setLoading(true);
-			await axios.delete(`/api/${params.storeId}/products/${params.productId}`);
-			router.refresh();
-			router.push(`/${params.storeId}/products`);
-			toast.success('Product deleted.');
-		} catch (error: any) {
-			toast.error('Something went wrong.');
-		} finally {
-			setLoading(false);
-			setOpen(false);
+		if (!params.productId) {
+			return new NextResponse('Product id is required', { status: 400 });
 		}
-	};
 
-	return (
-		<>
-			<AlertModal
-				isOpen={open}
-				onClose={() => setOpen(false)}
-				onConfirm={onDelete}
-				loading={loading}
-			/>
-			<div className='flex items-center justify-between'>
-				<Heading title={title} description={description} />
-				{initialData && (
-					<Button
-						disabled={loading}
-						variant='destructive'
-						size='sm'
-						onClick={() => setOpen(true)}>
-						<Trash className='h-4 w-4' />
-					</Button>
-				)}
-			</div>
-			<Separator />
-			<Form {...form}>
-				<form
-					onSubmit={form.handleSubmit(onSubmit)}
-					className='space-y-8 w-full'>
-					<FormField
-						control={form.control}
-						name='images'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Images</FormLabel>
-								<FormControl>
-									<ImageUpload
-										value={field.value.map((image) => image.url)}
-										disabled={loading}
-										onChange={(url) =>
-											field.onChange([...field.value, { url }])
-										}
-										onRemove={(url) =>
-											field.onChange([
-												...field.value.filter((current) => current.url !== url),
-											])
-										}
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<div className='md:grid md:grid-cols-3 gap-8'>
-						<FormField
-							control={form.control}
-							name='name'
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Name</FormLabel>
-									<FormControl>
-										<Input
-											disabled={loading}
-											placeholder='Product name'
-											{...field}
-										/>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name='price'
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Price</FormLabel>
-									<FormControl>
-										<Input
-											type='number'
-											disabled={loading}
-											placeholder='9.99'
-											{...field}
-										/>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name='categoryId'
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Category</FormLabel>
-									<Select
-										disabled={loading}
-										onValueChange={field.onChange}
-										value={field.value}
-										defaultValue={field.value}>
-										<FormControl>
-											<SelectTrigger>
-												<SelectValue
-													defaultValue={field.value}
-													placeholder='Select a category'
-												/>
-											</SelectTrigger>
-										</FormControl>
-										<SelectContent>
-											{categories.map((category) => (
-												<SelectItem key={category.id} value={category.id}>
-													{category.name}
-												</SelectItem>
-											))}
-										</SelectContent>
-									</Select>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name='makeId'
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Make</FormLabel>
-									<Select
-										disabled={loading}
-										onValueChange={field.onChange}
-										value={field.value}
-										defaultValue={field.value}>
-										<FormControl>
-											<SelectTrigger>
-												<SelectValue
-													defaultValue={field.value}
-													placeholder='Select a make'
-												/>
-											</SelectTrigger>
-										</FormControl>
-										<SelectContent>
-											{makes.map((make) => (
-												<SelectItem key={make.id} value={make.id}>
-													{make.name}
-												</SelectItem>
-											))}
-										</SelectContent>
-									</Select>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name='yearId'
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Year</FormLabel>
-									<Select
-										disabled={loading}
-										onValueChange={field.onChange}
-										value={field.value}
-										defaultValue={field.value}>
-										<FormControl>
-											<SelectTrigger>
-												<SelectValue
-													defaultValue={field.value}
-													placeholder='Select a year'
-												/>
-											</SelectTrigger>
-										</FormControl>
-										<SelectContent>
-											{years.map((year) => (
-												<SelectItem key={year.id} value={year.id}>
-													{year.name}
-												</SelectItem>
-											))}
-										</SelectContent>
-									</Select>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name='colorId'
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Color</FormLabel>
-									<Select
-										disabled={loading}
-										onValueChange={field.onChange}
-										value={field.value}
-										defaultValue={field.value}>
-										<FormControl>
-											<SelectTrigger>
-												<SelectValue
-													defaultValue={field.value}
-													placeholder='Select a color'
-												/>
-											</SelectTrigger>
-										</FormControl>
-										<SelectContent>
-											{colors.map((color) => (
-												<SelectItem key={color.id} value={color.id}>
-													{color.name}
-												</SelectItem>
-											))}
-										</SelectContent>
-									</Select>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name='isFeatured'
-							render={({ field }) => (
-								<FormItem className='flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4'>
-									<FormControl>
-										<Checkbox
-											checked={field.value}
-											// @ts-ignore
-											onCheckedChange={field.onChange}
-										/>
-									</FormControl>
-									<div className='space-y-1 leading-none'>
-										<FormLabel>Featured</FormLabel>
-										<FormDescription>
-											This product will appear on the home page
-										</FormDescription>
-									</div>
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name='isArchived'
-							render={({ field }) => (
-								<FormItem className='flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4'>
-									<FormControl>
-										<Checkbox
-											checked={field.value}
-											// @ts-ignore
-											onCheckedChange={field.onChange}
-										/>
-									</FormControl>
-									<div className='space-y-1 leading-none'>
-										<FormLabel>Archived</FormLabel>
-										<FormDescription>
-											This product will not appear anywhere in the store.
-										</FormDescription>
-									</div>
-								</FormItem>
-							)}
-						/>
-					</div>
-					<Button disabled={loading} className='ml-auto' type='submit'>
-						{action}
-					</Button>
-				</form>
-			</Form>
-		</>
-	);
-};
+		const storeByUserId = await prismadb.store.findFirst({
+			where: {
+				id: params.storeId,
+				userId,
+			},
+		});
+
+		if (!storeByUserId) {
+			return new NextResponse('Unauthorized', { status: 405 });
+		}
+
+		const product = await prismadb.product.delete({
+			where: {
+				id: params.productId,
+			},
+		});
+
+		return NextResponse.json(product);
+	} catch (error) {
+		console.log('[PRODUCT_DELETE]', error);
+		return new NextResponse('Internal error', { status: 500 });
+	}
+}
+
+export async function PATCH(
+	req: Request,
+	{ params }: { params: { productId: string; storeId: string } },
+) {
+	try {
+		const { userId } = auth();
+
+		const body = await req.json();
+
+		const {
+			name,
+			price,
+			categoryId,
+			images,
+			colorId,
+			makeId,
+			yearId,
+			conditionId,
+			driveTypeId,
+			engineVolumeId,
+			fuelTypeId,
+			locationId,
+			modelId,
+			optionId,
+			passengerId,
+			steeringId,
+			transmissionId,
+			isFeatured,
+			isArchived,
+		} = body;
+
+		if (!userId) {
+			return new NextResponse('Unauthenticated', { status: 403 });
+		}
+
+		if (!params.productId) {
+			return new NextResponse('Product id is required', { status: 400 });
+		}
+
+		if (!name) {
+			return new NextResponse('Name is required', { status: 400 });
+		}
+
+		if (!images || !images.length) {
+			return new NextResponse('Images are required', { status: 400 });
+		}
+
+		if (!price) {
+			return new NextResponse('Price is required', { status: 400 });
+		}
+
+		if (!categoryId) {
+			return new NextResponse('Category id is required', { status: 400 });
+		}
+
+		if (!colorId) {
+			return new NextResponse('Color id is required', { status: 400 });
+		}
+
+		if (!makeId) {
+			return new NextResponse('Make id is required', { status: 400 });
+		}
+
+		if (!yearId) {
+			return new NextResponse('Year id is required', { status: 400 });
+		}
+
+		if (!conditionId) {
+			return new NextResponse('Condition id is required', { status: 400 });
+		}
+
+		if (!driveTypeId) {
+			return new NextResponse('Drive Type id is required', { status: 400 });
+		}
+
+		if (!engineVolumeId) {
+			return new NextResponse('Engine Volume id is required', { status: 400 });
+		}
+
+		if (!fuelTypeId) {
+			return new NextResponse('Fuel Type id is required', { status: 400 });
+		}
+
+		if (!locationId) {
+			return new NextResponse('Location id is required', { status: 400 });
+		}
+
+		if (!modelId) {
+			return new NextResponse('Model id is required', { status: 400 });
+		}
+
+		if (!optionId) {
+			return new NextResponse('Option id is required', { status: 400 });
+		}
+
+		if (!passengerId) {
+			return new NextResponse('Passenger id is required', { status: 400 });
+		}
+
+		if (!steeringId) {
+			return new NextResponse('Steering id is required', { status: 400 });
+		}
+
+		if (!transmissionId) {
+			return new NextResponse('Transmission id is required', { status: 400 });
+		}
+
+		const storeByUserId = await prismadb.store.findFirst({
+			where: {
+				id: params.storeId,
+				userId,
+			},
+		});
+
+		if (!storeByUserId) {
+			return new NextResponse('Unauthorized', { status: 405 });
+		}
+
+		await prismadb.product.update({
+			where: {
+				id: params.productId,
+			},
+			data: {
+				name,
+				price,
+				categoryId,
+				colorId,
+				makeId,
+				yearId,
+				conditionId,
+				driveTypeId,
+				engineVolumeId,
+				fuelTypeId,
+				locationId,
+				modelId,
+				optionId,
+				passengerId,
+				steeringId,
+				transmissionId,
+				images: {
+					deleteMany: {},
+				},
+				isFeatured,
+				isArchived,
+			},
+		});
+
+		const product = await prismadb.product.update({
+			where: {
+				id: params.productId,
+			},
+			data: {
+				images: {
+					createMany: {
+						data: [...images.map((image: { url: string }) => image)],
+					},
+				},
+			},
+		});
+
+		return NextResponse.json(product);
+	} catch (error) {
+		console.log('[PRODUCT_PATCH]', error);
+		return new NextResponse('Internal error', { status: 500 });
+	}
+}
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -149,22 +149,22 @@ export function MainNav({
 	}, []);
 
   return (
-    <nav
-      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
-      {...props}
-    >
-      {routes.map((route) => (
-        <Link
-          key={route.href}
-          href={route.href}
-          className={cn(
-            'text-sm font-medium transition-colors hover:text-primary',
-            route.active ? 'text-black dark:text-white' : 'text-muted-foreground'
-          )}
-        >
-          {route.label}
-      </Link>
-      ))}
-    </nav>
-  )
+		<nav
+			className={cn('flex items-center space-x-4 lg:space-x-6', className)}
+			{...props}>
+			{allRoutes.map((route) => (
+				<Link
+					key={route.href}
+					href={route.href}
+					className={cn(
+						'text-sm font-medium transition-colors hover:text-primary',
+						route.active
+							? 'text-black dark:text-white'
+							: 'text-muted-foreground',
+					)}>
+					{route.label}
+				</Link>
+			))}
+		</nav>
+	);
 };
