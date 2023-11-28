90b272ec2cbd216d319331ad47f487fcedf3a18d
vvsuji <suji.falcon@gmail.com>
2023-11-27 21:55:17 -0500

    FIXED LOCATION?

--- app/(dashboard)/[storeId]/(routes)/locations/[locationId]/components/location-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/locations/[locationId]/components/location-form.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-<<<<<<<< HEAD:app/(dashboard)/[storeId]/(routes)/locations/[locationId]/components/location-form.tsx
 import * as z from 'zod';
 import axios from 'axios';
 import { useState } from 'react';
@@ -10,17 +9,6 @@ import { toast } from 'react-hot-toast';
 import { Trash } from 'lucide-react';
 import { Location } from '@prisma/client';
 import { useParams, useRouter } from 'next/navigation';
-========
-import * as z from "zod"
-import axios from "axios"
-import { useState } from "react"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { toast } from "react-hot-toast"
-import { Trash } from "lucide-react"
-import { Year } from '@prisma/client';
-import { useParams, useRouter } from "next/navigation"
->>>>>>>> 53f550f856d54e612a3c99786edcda9ac800bf03:app/(dashboard)/[storeId]/(routes)/years/[yearId]/components/year-form.tsx
 
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
@@ -41,7 +29,6 @@ const formSchema = z.object({
 	// value: z.string().min(1),
 });
 
-<<<<<<<< HEAD:app/(dashboard)/[storeId]/(routes)/locations/[locationId]/components/location-form.tsx
 type LocationFormValues = z.infer<typeof formSchema>;
 
 interface LocationFormProps {
@@ -49,52 +36,29 @@ interface LocationFormProps {
 }
 
 export const LocationForm: React.FC<LocationFormProps> = ({ initialData }) => {
-========
-type YearFormValues = z.infer<typeof formSchema>;
-
-interface YearFormProps {
-	initialData: Year | null;
-};
-
-export const YearForm: React.FC<YearFormProps> = ({ initialData }) => {
->>>>>>>> 53f550f856d54e612a3c99786edcda9ac800bf03:app/(dashboard)/[storeId]/(routes)/years/[yearId]/components/year-form.tsx
 	const params = useParams();
 	const router = useRouter();
 
 	const [open, setOpen] = useState(false);
 	const [loading, setLoading] = useState(false);
 
-<<<<<<<< HEAD:app/(dashboard)/[storeId]/(routes)/locations/[locationId]/components/location-form.tsx
 	const title = initialData ? 'Edit location' : 'Create location';
 	const description = initialData ? 'Edit a location.' : 'Add a new location';
 	const toastMessage = initialData ? 'Location updated.' : 'Location created.';
 	const action = initialData ? 'Save changes' : 'Create';
 
 	const form = useForm<LocationFormValues>({
-========
-	const title = initialData ? 'Edit year' : 'Create year';
-	const description = initialData ? 'Edit a year.' : 'Add a new year';
-	const toastMessage = initialData ? 'Year updated.' : 'Year created.';
-	const action = initialData ? 'Save changes' : 'Create';
-
-	const form = useForm<YearFormValues>({
->>>>>>>> 53f550f856d54e612a3c99786edcda9ac800bf03:app/(dashboard)/[storeId]/(routes)/years/[yearId]/components/year-form.tsx
 		resolver: zodResolver(formSchema),
 		defaultValues: initialData || {
 			name: '',
 		},
 	});
 
-<<<<<<<< HEAD:app/(dashboard)/[storeId]/(routes)/locations/[locationId]/components/location-form.tsx
 	const onSubmit = async (data: LocationFormValues) => {
-========
-	const onSubmit = async (data: YearFormValues) => {
->>>>>>>> 53f550f856d54e612a3c99786edcda9ac800bf03:app/(dashboard)/[storeId]/(routes)/years/[yearId]/components/year-form.tsx
 		try {
 			setLoading(true);
 			if (initialData) {
 				await axios.patch(
-<<<<<<<< HEAD:app/(dashboard)/[storeId]/(routes)/locations/[locationId]/components/location-form.tsx
 					`/api/${params.storeId}/locations/${params.locationId}`,
 					data,
 				);
@@ -103,16 +67,6 @@ export const YearForm: React.FC<YearFormProps> = ({ initialData }) => {
 			}
 			router.refresh();
 			router.push(`/${params.storeId}/locations`);
-========
-					`/api/${params.storeId}/years/${params.yearId}`,
-					data,
-				);
-			} else {
-				await axios.post(`/api/${params.storeId}/years`, data);
-			}
-			router.refresh();
-			router.push(`/${params.storeId}/years`);
->>>>>>>> 53f550f856d54e612a3c99786edcda9ac800bf03:app/(dashboard)/[storeId]/(routes)/years/[yearId]/components/year-form.tsx
 			toast.success(toastMessage);
 		} catch (error: any) {
 			toast.error('Something went wrong.');
@@ -124,7 +78,6 @@ export const YearForm: React.FC<YearFormProps> = ({ initialData }) => {
 	const onDelete = async () => {
 		try {
 			setLoading(true);
-<<<<<<<< HEAD:app/(dashboard)/[storeId]/(routes)/locations/[locationId]/components/location-form.tsx
 			await axios.delete(
 				`/api/${params.storeId}/locations/${params.locationId}`,
 			);
@@ -135,14 +88,6 @@ export const YearForm: React.FC<YearFormProps> = ({ initialData }) => {
 			toast.error(
 				'Make sure you removed all products using this location first.',
 			);
-========
-			await axios.delete(`/api/${params.storeId}/years/${params.yearId}`);
-			router.refresh();
-			router.push(`/${params.storeId}/years`);
-			toast.success('Year deleted.');
-		} catch (error: any) {
-			toast.error('Make sure you removed all products using this year first.');
->>>>>>>> 53f550f856d54e612a3c99786edcda9ac800bf03:app/(dashboard)/[storeId]/(routes)/years/[yearId]/components/year-form.tsx
 		} finally {
 			setLoading(false);
 			setOpen(false);
@@ -184,11 +129,7 @@ export const YearForm: React.FC<YearFormProps> = ({ initialData }) => {
 									<FormControl>
 										<Input
 											disabled={loading}
-<<<<<<<< HEAD:app/(dashboard)/[storeId]/(routes)/locations/[locationId]/components/location-form.tsx
 											placeholder='Location name'
-========
-											placeholder='Year name'
->>>>>>>> 53f550f856d54e612a3c99786edcda9ac800bf03:app/(dashboard)/[storeId]/(routes)/years/[yearId]/components/year-form.tsx
 											{...field}
 										/>
 									</FormControl>
@@ -196,27 +137,6 @@ export const YearForm: React.FC<YearFormProps> = ({ initialData }) => {
 								</FormItem>
 							)}
 						/>
-						{/* <FormField
-							control={form.control}
-							name='value'
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Value</FormLabel>
-									<FormControl>
-										<Input
-											disabled={loading}
-<<<<<<<< HEAD:app/(dashboard)/[storeId]/(routes)/locations/[locationId]/components/location-form.tsx
-											placeholder='Location value'
-========
-											placeholder='Year value'
->>>>>>>> 53f550f856d54e612a3c99786edcda9ac800bf03:app/(dashboard)/[storeId]/(routes)/years/[yearId]/components/year-form.tsx
-											{...field}
-										/>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/> */}
 					</div>
 					<Button disabled={loading} className='ml-auto' type='submit'>
 						{action}
