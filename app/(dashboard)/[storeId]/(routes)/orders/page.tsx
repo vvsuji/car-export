2653747b8ca2cb55ca715ba2f3e539e3b3917177
vvsuji <suji.falcon@gmail.com>
2023-12-28 00:02:12 -0500

    test

--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -29,16 +29,20 @@ const OrdersPage = async ({
   });
 
   const formattedOrders: OrderColumn[] = orders.map((item) => ({
-    id: item.id,
-    phone: item.phone,
-    address: item.address,
-    products: item.orderItems.map((orderItem) => orderItem.product.name).join(', '),
-    totalPrice: formatter.format(item.orderItems.reduce((total, item) => {
-      return total + Number(item.product.price)
-    }, 0)),
-    isPaid: item.isPaid,
-    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
-  }));
+		id: item.id,
+		phone: item.phone,
+		address: item.address,
+		products: item.orderItems
+			.map((orderItem) => orderItem.product.model)
+			.join(', '),
+		totalPrice: formatter.format(
+			item.orderItems.reduce((total, item) => {
+				return total + Number(item.product.price);
+			}, 0),
+		),
+		isPaid: item.isPaid,
+		createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+	}));
 
   return (
     <div className="flex-col">
