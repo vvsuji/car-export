cbc8154a3d3ecf96052e81408b09e69a425e47da
vvsuji <suji.falcon@gmail.com>
2023-12-28 00:12:15 -0500

    maybe?

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -40,7 +40,7 @@ export async function POST(
 			price_data: {
 				currency: 'KRW',
 				product_data: {
-					name: product.name,
+					name: product.model,
 				},
 				unit_amount: product.price.toNumber() * 100,
 			},
