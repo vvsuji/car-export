068a4462684c03dc9aff040f1f24690f63ea485c
vvsuji <suji.falcon@gmail.com>
2024-01-20 13:20:18 -0500

    deleted irrelevant code

--- app/(dashboard)/[storeId]/(routes)/options/[optionId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/options/[optionId]/page.tsx
@@ -1,35 +1,3 @@
-// import prismadb from "@/lib/prismadb";
-
-// import { OptionForm } from './components/options-form';
-
-// const OptionsPage = async ({ params }: { params: { option: string[] } }) => {
-// 	// Assuming optionId is an array of strings
-// 	const optionIds = params.option;
-
-// 	const options = await prismadb.option.findMany({
-// 		where: {
-// 			id: {
-// 				in: optionIds,
-// 			},
-// 		},
-// 	});
-
-// 	// Handling multiple options returned from the database
-// 	// You might need to adjust the OptionForm component
-// 	// to accept and display multiple options if necessary
-// 	return (
-// 		<div className='flex-col'>
-// 			<div className='flex-1 space-y-4 p-8 pt-6'>
-// 				{options.map((option) => (
-// 					<OptionForm key={option.id} initialData={option} />
-// 				))}
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default OptionsPage;
-
 import prismadb from '@/lib/prismadb';
 
 import { OptionForm } from './components/options-form';
